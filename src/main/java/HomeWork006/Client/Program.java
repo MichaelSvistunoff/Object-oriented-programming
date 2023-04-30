11366674cd368ab3fb0bd9b9ae4f5b4555480527
MichaelSv <michaelsvistunoff@gmail.com>
2023-05-01 04:03:27 +0600

    Initial commit HW6

---
+++ src/main/java/HomeWork006/Client/Program.java
@@ -0,0 +1,9 @@
+package HomeWork006.Client;
+
+import HomeWork006.UI.App;
+
+public class Program {
+    public static void main(String[] args) {
+        App.ButtonClick();
+    }
+}
---
+++ src/main/java/HomeWork006/Config.java
@@ -0,0 +1,5 @@
+package HomeWork006;
+
+public class Config {
+    public static String pathDb = "src/main/java/HomeWork005/data.db";
+}
